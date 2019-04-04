849b6bf30d1b11a965960ad6404706320954b7bd
Copyes <fan_545697167@qq.com>
2019-04-04 21:34:28 +0800

    :tada: initial commits

---
+++ dist/rollup_startup.esm.js
@@ -0,0 +1,6 @@
+/*!
+ * rollup_startup.js v1.0.0
+ * (c) 2018-2019 Fan Chao
+ * Released under the MIT License.
+ */
+console.log(1111);
---
+++ dist/rollup_startup.js
@@ -0,0 +1,13 @@
+/*!
+ * rollup_startup.js v1.0.0
+ * (c) 2018-2019 Fan Chao
+ * Released under the MIT License.
+ */
+(function (factory) {
+	typeof define === 'function' && define.amd ? define(factory) :
+	factory();
+}(function () { 'use strict';
+
+	console.log(1111);
+
+}));
